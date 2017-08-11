2ae2d3a64b16247d5aa3412404527381c130a10e
AKAK123 <315719695@qq.com>
2017-08-11 10:04:59 +0800

    delete ypbZF

--- yupiaobaoZF/src/com/szcares/bean/IGrabTicket.java
+++
@@ -1,8 +0,0 @@
-package com.szcares.bean;
-
-import java.util.HashMap;
-
-public interface IGrabTicket {
-	public HashMap<String, Object> grabTicketNotice(HashMap<String, Object> request);
-	public HashMap<String, Object> addGrabTicketPlan(HashMap<String, Object> request);
-}
